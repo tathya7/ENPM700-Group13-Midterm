b87eee6f91fd61ff0626b5eb1dd15228a301f8ba
mun0404 <munawwar4400@gmail.com>
2024-10-16 23:29:54 -0400

    In this commit we have created a header file for robot controller.

---
+++ include/robot_controller.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <utility>
+
+/**
+ * @brief RobotController class to control robot movement based on avoidance strategy
+ */
+class RobotController {
+ private:
+  /**
+   * @brief Current position of the robot
+   */
+  std::pair<double, double> current_position;
+
+  /**
+   * @brief Target position of the robot
+   */
+  std::pair<double, double> target_position;
+
+  /**
+   * @brief Maximum allowable speed for the robot
+   */
+  double max_speed;
+
+  /**
+   * @brief Function to adjust the robot's path based on avoidance requirements
+   * 
+   * @param is_avoidance_required Indicates if avoidance is necessary
+   */
+  void adjust_path(bool is_avoidance_required);
+
+ public:
+  /**
+   * @brief Construct a new RobotController object
+   * 
+   * @param start_position Initial position of the robot
+   * @param max_speed Maximum allowable speed
+   */
+  RobotController(const std::pair<double, double>& start_position, double max_speed);
+
+  /**
+   * @brief Set the target position for the robot
+   * 
+   * @param target Target position
+   */
+  void set_target_position(const std::pair<double, double>& target);
+
+  /**
+   * @brief Move the robot towards the target while avoiding obstacles
+   * 
+   * @param is_avoidance_required Flag indicating if avoidance is necessary
+   * @return std::pair<double, double> New position of the robot
+   */
+  std::pair<double, double> move_robot(bool is_avoidance_required);
+};
