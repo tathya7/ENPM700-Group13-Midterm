2de5081459f5762f3bc31b316179143748ef8db2
mun0404 <munawwar4400@gmail.com>
2024-10-24 00:01:00 -0400

    Deleted unwanted files.

--- build/test/cpp-test[1]_tests.cmake
+++
@@ -1,7 +0,0 @@
-add_test( dummy_test.this_should_pass /home/tathyab/ENPM700-Group13-Midterm/build/test/cpp-test [==[--gtest_filter=dummy_test.this_should_pass]==] --gtest_also_run_disabled_tests)
-set_tests_properties( dummy_test.this_should_pass PROPERTIES WORKING_DIRECTORY /home/tathyab/ENPM700-Group13-Midterm/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test( dummy_test.this_should_pass_too /home/tathyab/ENPM700-Group13-Midterm/build/test/cpp-test [==[--gtest_filter=dummy_test.this_should_pass_too]==] --gtest_also_run_disabled_tests)
-set_tests_properties( dummy_test.this_should_pass_too PROPERTIES WORKING_DIRECTORY /home/tathyab/ENPM700-Group13-Midterm/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test( dummy_test.this_will_fail /home/tathyab/ENPM700-Group13-Midterm/build/test/cpp-test [==[--gtest_filter=dummy_test.this_will_fail]==] --gtest_also_run_disabled_tests)
-set_tests_properties( dummy_test.this_will_fail PROPERTIES WORKING_DIRECTORY /home/tathyab/ENPM700-Group13-Midterm/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( cpp-test_TESTS dummy_test.this_should_pass dummy_test.this_should_pass_too dummy_test.this_will_fail)
