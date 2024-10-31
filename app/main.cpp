24e6e9600c11dbdbcb4f87f20e9c403ab59bd3f3
tathya7 <tathyabhatt348@gmail.com>
2024-10-30 21:58:55 -0400

    Updated the human avoidance header script after refactoring the implementations in human_avoidance.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
+#include <ostream>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/opencv.hpp>
-#include <ostream>
-#include "human_detector.hpp"
+
 #include "human_avoidance.hpp"
+#include "human_detector.hpp"
 
-    
 int main(int argc, char** argv) {
-    std::string camera_device = argv[1];
+  std::string camera_device = argv[1];
 
-    HumanDetector detection;
+  HumanDetector detection;
 
-    while (1) {
-     detection.detect(camera_device);
-     }
+  while (1) {
+    detection.detect(camera_device);
+  }
 
-    return 0;
-
+  return 0;
 }
--- include/human_avoidance.hpp
+++ include/human_avoidance.hpp
@@ -2,32 +2,52 @@
 
 #include <array>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <string>
 #include <vector>
-#include <opencv2/opencv.hpp>
 
+/**
+ * @brief A class for human avoidance functionality
+ *
+ * The HumanAvoidance class provides functionality for detecting humans
+ * and calculating their positions relative to a robot's coordinate system.
+ * Further, if a human is detected around a certain radius, the red marker
+ * is shown giving the warning in the output.
+ */
 class HumanAvoidance {
+ private:
+
+  const unsigned int averageHeight = 175;  // Average human height in cm
+
+ public:
+  int frame_id;
+
   
-  private:
-    // int SIZE;
-    // int object_id;
-    // int float_id;
-    const unsigned int averageHeight = 175; // Average human height in cm
+  /**
+   * @brief Default constructor for the class
+   */
+  HumanAvoidance();
 
-  public:
-    int frame_id;
+  /**
+   * @brief Calculates the distance of a detected human from the camera
+   *
+   * @param box_h Height of the bounding box around the detected human
+   * @param frame_h Input frame height
+   * @return float The calculated distance in meters
+   */  
+   
+   float calculate_distance(int box_h, int frame_h);
 
-    // Constructor and Destructor
-    HumanAvoidance();
+  /**
+   * @brief Transforms human coordinates from camera to robot coordinate system
+   *
+   * @param z Distance of the human from the camera
+   * @param box Bounding box of the detected human in the camera frame
+   * @param frame The current video frame
+   * @return std::vector<float> A vector containing the x, y, z coordinates in the robot's frame
+   */  
+   
+   std::vector<float> camera2robot(float z, cv::Rect box, cv::Mat frame);
 
-    // Calculates distance of the detected human from the camera
-    float calculate_distance(int box_h, int frame_h);
-
-    // Transforms human coordinates to robot coordinate system
-    std::vector<float> camera2robot(float z, cv::Rect box, cv::Mat frame);
-
-    // void avoidance_(cv::rect box, cv::Mat frame);
-
-    ~HumanAvoidance();
-
+  ~HumanAvoidance();
 };
